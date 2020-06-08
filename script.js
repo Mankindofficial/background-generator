bd6f6f0c77c9516118b90c76fb31c613507462b2
mankind <mankindofficial1@gmail.com>
2020-06-08 18:16:17 +0100

    Updated Project

--- script.js
+++ script.js
@@ -3,6 +3,23 @@ const input2 = document.querySelector(".color2");
 const h3 = document.querySelector("h3");
 const body = document.querySelector("#bodyy");
 
+const getRandomColor = () => {
+  var letters = '0123456789ABCDEF';
+  var color = '#';
+  for (var i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
+const setRandomColor = () => {
+	input1.value = getRandomColor();
+	input2.value = getRandomColor();
+	body.style.background = `linear-gradient(to right, ${input1.value}, ${input2.value})`;
+	h3.textContent = body.style.background;
+}
+
+setRandomColor();
+
 const changeBackground = () => {
 	body.style.background = `linear-gradient(to right, ${input1.value}, ${input2.value})`;
 	h3.textContent = body.style.background;
